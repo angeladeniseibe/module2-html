0ed3d1018abb21028f745129e636d94c44ed4dba
angeladeniseibe <angeladeniseibe@gmail.com>
2026-03-26 12:05:17 +0800

    WSAT-M7: Connected React to PHP API

---
+++ personal-cv-react/src/components/Card.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function Card({ title, children }) {
+    return (
+        <section className="card">
+            <h2>{title}</h2>
+            {children}
+        </section>
+    );
+}
+export default Card;
